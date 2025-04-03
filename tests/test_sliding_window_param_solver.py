1db374c91c8fcbf9315a1bef0be0f55f3df24bbf
CorentinJ <corentin.jemine@gmail.com>
2025-04-03 14:29:47 +0200

    feat(solver): Three new tests

--- tests/test_sliding_window_param_solver.py
+++ tests/test_sliding_window_param_solver.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import List, Tuple
 
 import numpy as np
 import pytest
@@ -84,9 +84,89 @@ def test_conv_transpose_1d(kernel_size: int, stride: int, dilation: int):
     ), f"Expected solution with {params_str}, but none found in {sols}"
 
 
+@pytest.mark.parametrize(
+    "conv_params",
+    [
+        (
+            {"transposed": False, "kernel_size": 7, "stride": 1, "padding": 3, "dilation": 1},
+            {"transposed": False, "kernel_size": 3, "stride": 1, "padding": 2, "dilation": 2},
+            {"transposed": True, "kernel_size": 2, "stride": 2},
+            {"transposed": True, "kernel_size": 3, "stride": 3},
+        ),
+    ],
+)
+def test_conv_mix(conv_params: List[Tuple[dict]]):
+    set_seed(0x5EED)
+
+    network = nn.Sequential(
+        *[
+            (nn.ConvTranspose1d if params["transposed"] else nn.Conv1d)(
+                in_channels=1,
+                out_channels=1,
+                kernel_size=params["kernel_size"],
+                stride=params["stride"],
+                padding=params.get("padding", 0),
+                dilation=params.get("dilation", 1),
+            )
+            for params in conv_params
+        ]
+    )
+    sols = find_sliding_window_params_for_transform(network, TensorSpec(shape=(1, 1, -1)))
+
+    # TODO: include expected parameters
+    assert sols, f"Expected solution, but none found for {network}"
+
+
 def test_infinite_receptive_field():
+    """
+    Tests that the solver does not find a solution for a transform that has an infinite receptive field.
+    """
+
     def transform(x: np.ndarray):
         return np.full_like(x, fill_value=np.mean(x))
+
+    sols = find_sliding_window_params_for_transform(transform, TensorSpec(shape=(-1,), dtype=np.float32))
+    assert not sols
+
+
+def test_no_receptive_field():
+    """
+    Tests that the solver does not find a solution for a transform that has no receptive field (output is not
+    a function of the input).
+    """
+
+    def transform(x: np.ndarray):
+        return np.full_like(x, fill_value=3.0)
+
+    sols = find_sliding_window_params_for_transform(transform, TensorSpec(shape=(-1,), dtype=np.float32))
+    assert not sols
+
+
+@pytest.mark.parametrize("variant", ["prefix_mean", "suffix_mean", "mod7"])
+def test_variable_receptive_field(variant: str):
+    """
+    Tests that the solver does not find a solution for a transform that has a receptive field of variable size.
+
+    NOTE: these could technically be handled
+    """
+    if variant in ("prefix_mean", "suffix_mean"):
+        pytest.skip("These cases are not properly handled yet")
+
+    def transform(x: np.ndarray):
+        y = np.zeros_like(x)
+
+        if variant == "prefix_mean":
+            for i in range(len(x)):
+                y[i] = np.mean(x[: i + 1])
+        elif variant == "suffix_mean":
+            for i in range(len(x)):
+                y[i] = np.mean(x[i:])
+        elif variant == "mod7":
+            for i in range(len(x)):
+                ksize = (i % 7) + 1
+                y[i] = np.mean(x[i : i + ksize])
+
+        return y
 
     sols = find_sliding_window_params_for_transform(transform, TensorSpec(shape=(-1,), dtype=np.float32))
     assert not sols
