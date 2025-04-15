5a8bc8cbb90cdfbe0785399bff666a1b6c7b9d18
CorentinJ <corentin.jemine@gmail.com>
2025-04-15 16:18:47 +0200

    feat: More efficient sli stream

--- torchstream/sliding_window/sliding_window_stream.py
+++ torchstream/sliding_window/sliding_window_stream.py
@@ -1,3 +1,4 @@
+import math
 from typing import Callable, Optional, Tuple, Union
 
 import numpy as np
@@ -21,56 +22,37 @@ class SlidingWindowStream(Stream):
         self.transform = transform
         self.params = sliding_window_params
 
-        # self._n_left_wins_to_discard = 0
-        # self._input_size_needed = self.params.kernel_size_in - self.params.left_pad
-        self._n_eff_wins_out = 0
+        # Number of windows that are wasted on the left solely due to padding. "wasted" here means that we recompute
+        # these windows on each step despite them being unnecessary, where proper streaming would not recompute them.
+        self._n_left_wins_wasted = int(math.ceil(self.params.left_pad / self.params.stride_in))
+        self._n_extra_wins_to_buffer = self._n_left_wins_wasted
 
     # FIXME: signature
     def _step(self) -> Union[Tensor, np.ndarray, Tuple[Tensor, np.ndarray]]:
         # TODO: multi input support
         in_buff = self._in_buffs[0]
 
-        # # We need less inputs on the last step, because right padding will then be useful
-        # if in_buff.output_closed:
-        #     self._input_size_needed -= self.params.right_pad
-        # if in_buff.size < self._input_size_needed:
-        #     # TODO
-        #     raise RuntimeError()
-
         (left_pad, right_pad), num_wins, expected_out_size = self.params.get_metrics_for_input(in_buff.size)
-        # if not num_wins:
-        #     raise NotEnoughInputError(
-        #         f"Need a sequence of size {self.params.get_min_input_size()} to produce any output, "
-        #         f"got a sequence of size {in_buff.size}."
-        #     )
 
-        eff_num_wins = 0
-        out_trim_start = None
-        out_trim_end = None
-        for win_idx in range(num_wins):
-            in_sli = slice(
-                win_idx * self.params.stride_in, win_idx * self.params.stride_in + self.params.kernel_size_in
+        # Get the index of the first window that will compute valid output that we'll return
+        first_eff_win_idx = self._n_left_wins_wasted - self._n_extra_wins_to_buffer
+        out_trim_start = first_eff_win_idx * self.params.stride_out
+
+        # If the input is closed, there is no output trimming that needs to occur on the right side
+        if in_buff.input_closed:
+            out_trim_end = None
+            eff_num_wins = num_wins - first_eff_win_idx
+        # Otherwise, we need to trim the output where it would start being incorrect due to the right input padding
+        else:
+            last_eff_win_idx = int(
+                math.ceil(max(0, left_pad + in_buff.size - self.params.kernel_size_in) / self.params.stride_in)
             )
-            out_sli = slice(
-                win_idx * self.params.stride_out, win_idx * self.params.stride_out + self.params.kernel_size_out
-            )
+            out_trim_end = last_eff_win_idx * self.params.stride_out
+            eff_num_wins = last_eff_win_idx - first_eff_win_idx
 
-            if win_idx >= self._n_eff_wins_out:
-                if out_trim_start is None:
-                    out_trim_start = out_sli.start
-
-                if not in_buff.input_closed and in_sli.stop >= left_pad + in_buff.size:
-                    out_trim_end = out_sli.start
-                    break
-
-                eff_num_wins += 1
-
-        if not eff_num_wins:
-            raise NotEnoughInputError()
-
-        assert eff_num_wins
-        assert out_trim_end is None or out_trim_start < out_trim_end
-        self._n_eff_wins_out += eff_num_wins
+        if eff_num_wins <= 0:
+            # TODO: breakdown current state & display how much more data is needed
+            raise NotEnoughInputError(f"Sequence of size {in_buff.size} is not enough to produce any output.")
 
         # Forward the input
         tsfm_input = in_buff.peek()
@@ -82,6 +64,13 @@ class SlidingWindowStream(Stream):
                 f"sequence instead of {expected_out_size} for {in_buff.size} sized input. Sliding window params: "
                 f"{self.params}"
             )
+
+        # Drop input that won't be necessary in the future
+        if in_buff.input_closed:
+            in_buff._clear_buf()
+        elif eff_num_wins > self._n_extra_wins_to_buffer:
+            in_buff.drop((eff_num_wins - self._n_extra_wins_to_buffer) * self.params.stride_in)
+        self._n_extra_wins_to_buffer = max(0, self._n_extra_wins_to_buffer - eff_num_wins)
 
         # # We need to discard outputs on the left and right since this is a naive implementation
         # out_buff = StreamBuffer(tsfm_output, dim=-1)
