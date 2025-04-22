ded527d5711a6a5d628ce51f2d3e436f45b169ed
CorentinJ <corentin.jemine@gmail.com>
2025-04-22 10:24:49 +0200

    fix: is_similar_dtype() can now compare dtype instances and np.generic subclasses

--- torchstream/sequence_dtype.py
+++ torchstream/sequence_dtype.py
@@ -7,13 +7,13 @@ import torch
 seqdtype = Union[torch.dtype, np.dtype]
 
 
-# FIXME: name, "similar" is vague
+# FIXME: name, "similar" is vague -> "compatible"?
 @lru_cache
 def is_similar_dtype(dtype1: seqdtype, dtype2: seqdtype) -> bool:
     if isinstance(dtype1, torch.dtype) and isinstance(dtype2, torch.dtype):
         return _is_similar_torch_dtype(dtype1, dtype2)
-    if isinstance(dtype1, np.dtype) and isinstance(dtype2, np.dtype):
-        return _is_similar_numpy_dtype(dtype1, dtype2)
+    if _is_similar_numpy_dtype(dtype1, dtype2):
+        return True
     return False
 
 
@@ -39,5 +39,11 @@ def _is_similar_torch_dtype(dtype1: torch.dtype, dtype2: torch.dtype) -> bool:
     return False
 
 
-def _is_similar_numpy_dtype(dtype1: np.dtype, dtype2: np.dtype) -> bool:
-    return dtype1.kind == dtype2.kind
+def _is_similar_numpy_dtype(dtype1: np.dtype | type, dtype2: np.dtype | type) -> bool:
+    dtype1 = dtype1.type if isinstance(dtype1, np.dtype) else dtype1
+    dtype2 = dtype2.type if isinstance(dtype2, np.dtype) else dtype2
+
+    if not issubclass(dtype1, np.generic) or not issubclass(dtype2, np.generic):
+        return False
+
+    return np.dtype(dtype1).kind == np.dtype(dtype2).kind
