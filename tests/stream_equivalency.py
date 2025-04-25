7a9f70921e05e3389becaff0a336445d0c407163
CorentinJ <corentin.jemine@gmail.com>
2025-04-25 15:52:53 +0200

    feat: Template for desired Sequence API in stream equivalency

--- tests/stream_equivalency.py
+++ tests/stream_equivalency.py
@@ -1,14 +1,13 @@
 import itertools
-from typing import Callable, Optional, Tuple
+from typing import Callable, Tuple
 
 import numpy as np
 import pytest
 import torch
 
-from torchstream.buffers.stream_buffer import StreamBuffer
-from torchstream.sequence_spec import Sequence
-from torchstream.sliding_window.nan_trick import get_nan_range, set_nan_range
-from torchstream.stream import NotEnoughInputError, Stream
+from torchstream.sequence.sequence import Sequence
+from torchstream.sliding_window.nan_trick import get_nan_range
+from torchstream.stream import Stream
 
 
 @pytest.mark.skip("Not a test")
@@ -16,9 +15,8 @@ from torchstream.stream import NotEnoughInputError, Stream
 def test_stream_equivalent(
     sync_fn: Callable,
     stream: Stream,
-    input_provider: Optional[Callable[[int], Sequence]] = None,
+    in_seq: Sequence | None = None,
     in_step_sizes: Tuple[int, ...] = (7, 4, 12, 1, 17),
-    in_seq_size: int = 50,
     atol: float = 1e-5,
     check_throughput_with_nan_trick: bool = False,
 ):
@@ -31,81 +29,61 @@ def test_stream_equivalent(
 
     :param check_throughput_with_nan_trick: TODO: doc
     """
-    input_provider = input_provider or stream.in_spec.randn
-    sync_input = input_provider(in_seq_size)
+    if not in_seq:
+        in_seq = stream.in_spec.randn(seq_size=50)
 
-    ## Get the sync output
-    out_sync = sync_fn(sync_input)
-    out_sync_buff = StreamBuffer(stream.out_spec)
-    out_sync_buff.feed(out_sync, close_input=True)
-    out_size = out_sync_buff.size
+    # Get the sync output
+    # FIXME!: how's this gonna work for multiple inputs?
+    out_seq_ref = in_seq.apply(sync_fn, stream.out_spec)
 
-    ## Get the stream output & compare it
-    in_buff = StreamBuffer(stream.in_spec)
-    in_buff.feed(sync_input, close_input=True)
-    in_size = in_buff.size
-
-    # TODO: cleaner implementation of the throughput trick
-    stream_fed_seq_size = 0
-    nan_trick_in_buff = StreamBuffer(stream.in_spec)
-    nan_trick_in_buff.feed(sync_input)
     # FIXME: this is a trivial hack that assumes that the input size at least the kernel size, ideally we'd only
     # add the kernel size - 1 NaNs to the input.
-    nan_trick_in_buff.feed(sync_input, close_input=True)
-    nan_trick_in = nan_trick_in_buff.read()
-    set_nan_range(nan_trick_in, (in_size, None), dim=stream.in_spec.seq_dim)
+    in_nan_trick_seq = Sequence(in_seq, in_seq, close_input=True)
 
     step_size_iter = iter(itertools.cycle(in_step_sizes))
     i = 0
     while not stream.output_closed:
         step_size = next(step_size_iter)
-        stream_input_i = in_buff.read(step_size)
+        in_stream_i = in_seq.consume(step_size)
 
-        try:
-            out_stream_i = stream(stream_input_i, is_last_input=in_buff.output_closed)
-        except NotEnoughInputError:
-            out_stream_i = stream.out_spec.empty()
+        # FIXME: this is a seq
+        out_seq_stream_i = stream(in_stream_i, is_last_input=in_seq.output_closed, on_starve="empty")
 
-        stream_fed_seq_size += in_buff.spec.get_seq_size(stream_input_i)
-
-        stream_out_size = out_sync_buff.spec.get_seq_size(out_stream_i)
-        out_sync_i = out_sync_buff.read(stream_out_size)
+        out_sync_i = out_seq_ref.consume(out_seq_stream_i.size)
 
         # Ensure the outputs are close
-        assert out_sync_i.shape == out_stream_i.shape, (
-            f"Shape mismatch on step {i} (got {out_stream_i.shape}, expected {out_sync_i.shape})"
+        assert out_sync_i.shape == out_seq_stream_i.shape, (
+            f"Shape mismatch on step {i} (got {out_seq_stream_i.shape}, expected {out_sync_i.shape})"
         )
-        if stream_out_size:
-            max_error = np.abs(out_sync_i - out_stream_i).max()
+        if out_seq_stream_i.size:
+            max_error = np.abs(out_sync_i - out_seq_stream_i.data).max()
             assert max_error <= atol, (
                 f"Error too large on step {i} (got {max_error}, expected <= {atol})\n"
-                f"Sync: {out_sync_i}\nStream: {out_stream_i}"
+                f"Sync: {out_sync_i}\nStream: {out_seq_stream_i.data}"
             )
 
         # Check throughput with the NaN trick
         if check_throughput_with_nan_trick and not stream.output_closed:
-            # TODO: this api is terrible, let's improve it
-            copy_fn = np.copy if stream.in_spec.is_numpy else torch.clone
-            nan_trick_in_i = copy_fn(nan_trick_in)
-            set_nan_range(nan_trick_in_i, (stream_fed_seq_size, in_size), dim=stream.in_spec.seq_dim)
-            out_nan_trick_i = sync_fn(nan_trick_in_i)
-            out_nan_size = out_sync_buff.spec.get_seq_size(out_nan_trick_i)
-            nan_range = get_nan_range(out_nan_trick_i, dim=stream.out_spec.seq_dim)
+            in_nan_trick_seq_i = in_nan_trick_seq.copy()
+            in_nan_trick_seq_i[in_seq.consumed :] = float("nan")
+            # FIXME!: same as above
+            out_nan_trick_seq_i = in_nan_trick_seq_i.apply(sync_fn, stream.out_spec)
+            nan_range = get_nan_range(out_nan_trick_seq_i)
             assert nan_range, "Internal error: kernel size must be greater than the input sequence size"
 
-            assert nan_range[1] == out_nan_size, (
-                f"Transform is not suitable for NaN trick, NaNs set at {(stream_fed_seq_size, 2 * in_size)} in the "
-                f"input propagated up to {nan_range}, when the output is {out_nan_size} long. NaNs should have "
-                f"propagated to the end."
+            assert nan_range[1] == out_nan_trick_seq_i.size, (
+                # FIXME: can also happen if kernel is greater than the input size
+                f"Transform is not suitable for NaN trick, NaNs set at {(in_seq.consumed, in_nan_trick_seq.size)} in "
+                f"the input propagated up to {nan_range}, when the output is {out_nan_trick_seq_i.size} long. NaNs "
+                f"should have propagated to the end."
             )
 
-            current_output_size = out_size - out_sync_buff.size
-            assert nan_range[0] >= current_output_size, "Internal error"
-            assert nan_range[0] == current_output_size, (
+            assert nan_range[0] >= out_seq_ref.consumed, "Internal error"
+            assert nan_range[0] == out_seq_ref.consumed, (
                 f"The stream has output less than what's possible to output based on the NaN trick's output. "
-                f"Expected {nan_range[0]} outputs total at step {i}, got {current_output_size}."
+                f"Expected {nan_range[0]} outputs total at step {i}, got {out_seq_ref.consumed}."
             )
 
         i += 1
 
-    assert out_sync_buff.output_closed, f"Stream output is too short, {out_sync_buff.size} more outputs expected"
+    assert out_seq_ref.output_closed, f"Stream output is too short, {out_seq_ref.size} more outputs were expected"
