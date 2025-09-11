7d284356697844ead193015807e56789e183babc
CorentinJ <corentin.jemine@gmail.com>
2025-09-11 09:42:18 +0200

    fix: Rewrite of dummy sliding window transform to incorporate out trim

--- torchstream/sliding_window/dummy_sliding_window_transform.py
+++ torchstream/sliding_window/dummy_sliding_window_transform.py
@@ -16,16 +16,9 @@ class DummySlidingWindowTransform:
     def __call__(self, x: np.ndarray):
         (left_pad, right_pad), num_wins, out_size = self.params.get_metrics_for_input(x.shape[-1])
 
-        x = np.concatenate([np.zeros(left_pad), x])
-        if right_pad < 0:
-            x = x[:right_pad]
-        else:
-            x = np.concatenate([x, np.zeros(right_pad)])
-
         out = np.zeros(out_size)
-        for i in range(num_wins):
-            in_sli = slice(i * self.params.stride_in, i * self.params.stride_in + self.params.kernel_size_in)
-            out_sli = slice(i * self.params.stride_out, i * self.params.stride_out + self.params.kernel_size_out)
-            out[out_sli] += np.mean(x[in_sli])
+        for (in_start, in_stop), (out_start, out_stop) in self.params.iter_bounded_kernel_map(x.shape[-1]):
+            # NOTE: not weighing in the padding, but this is just a dummy transform so it's fine
+            out[out_start:out_stop] += np.mean(x[in_start:in_stop])
 
         return out
