38196d7f7a70c75568aa22196111947795f10836
CorentinJ <corentin.jemine@gmail.com>
2025-10-29 14:46:25 +0100

    feat: Call intercepter

--- examples/tts/demo.py
+++ examples/tts/demo.py
@@ -1,10 +1,47 @@
-# from diffusers import StableAudioPipeline
-# import torch
-
-# pipe = StableAudioPipeline.from_pretrained("stabilityai/stable-audio-open-1.0", torch_dtype=torch.float16).to("cuda")
-# audio = pipe(prompt="cinematic drum loop, 120 bpm, clean, punchy", audio_length_in_s=20).audios[0]
-
-
+import soundfile as sf
 import torch
+from kokoro import KPipeline
 
-print(torch.cuda.is_available())
+from torchstream.call_intercept import intercept
+
+# Change the tensor repr to show the shape and device, which saves a lot of time for debugging
+old_repr = torch.Tensor.__repr__
+torch.Tensor.__repr__ = lambda t: f"{tuple(t.shape)} {str(t.dtype).replace('torch.', '')} {str(t.device)} {old_repr(t)}"
+
+# uv pip install pip
+# .venv\Scripts\python.exe -m spacy download en_core_web_sm
+# spacy.load("en_core_web_sm")
+
+
+pipeline = KPipeline(lang_code="en-us", repo_id="hexgrad/Kokoro-82M")
+text = """
+[Kokoro](/kˈOkəɹO/) is an open-weight TTS model with 82 million parameters. Despite its lightweight architecture, it delivers comparable quality to larger models while being significantly faster and more cost-efficient. With Apache-licensed weights, [Kokoro](/kˈOkəɹO/) can be deployed anywhere from production environments to personal projects.
+"""
+
+# Normal, non-streaming inference
+# *_, audio = next(pipeline(text, voice="af_heart"))
+# sf.write("demo_audio.wav", audio, 24000)
+
+
+# Step 1: because kokoro is a third party library, we'll avoid modifying it's source code directly. Instead,
+# we'll monkey-patch the method where streaming can happen and work there.
+def mod_decoder_forward(self, asr, F0_curve, N, s):
+    F0 = self.F0_conv(F0_curve.unsqueeze(1))
+    N = self.N_conv(N.unsqueeze(1))
+    x = torch.cat([asr, F0, N], axis=1)
+    x = self.encode(x, s)
+    asr_res = self.asr_res(asr)
+    res = True
+    for block in self.decode:
+        if res:
+            x = torch.cat([x, asr_res, F0, N], axis=1)
+        x = block(x, s)
+        if block.upsample_type != "none":
+            res = False
+    x = self.generator(x, s, F0_curve)
+    return x
+
+
+with intercept("kokoro.istftnet.Decoder.forward") as calls:
+    *_, audio = next(pipeline(text, voice="af_heart"))
+    sf.write("demo_audio.wav", audio, 24000)
---
+++ torchstream/call_intercept.py
@@ -0,0 +1,61 @@
+import importlib
+from typing import Callable
+
+
+class _Intercept:
+    # TODO: 
+    #   - arg to intercept only once
+    #   - boolean for storing the call
+    #   - return value for the call?
+    def __init__(self, target: str, handler: Callable):
+        self._target = target
+        self._handler = handler
+
+        self._owner = None
+        self._attr_name = None
+        self._original = None
+
+    def __enter__(self):
+        parts = self._target.split(".")
+
+        module = None
+        remainder = None
+        for i in range(len(parts), 0, -1):
+            mod_name = ".".join(parts[:i])
+            try:
+                module = importlib.import_module(mod_name)
+            except ModuleNotFoundError:
+                continue
+            else:
+                remainder = parts[i:]
+                break
+
+        if module is None or remainder is None:
+            raise ImportError(f"Cannot resolve {self._target!r}")
+
+        obj = module
+        for name in remainder[:-1]:
+            obj = getattr(obj, name)
+
+        attr_name = remainder[-1]
+        original = getattr(obj, attr_name)
+
+        self._owner = obj
+        self._attr_name = attr_name
+        self._original = original
+
+        orig_fn = original
+
+        def wrapper(*args, **kwargs):
+            return self._handler(orig_fn, args, kwargs)
+
+        setattr(obj, attr_name, wrapper)
+        return self.calls
+
+    def __exit__(self, exc_type, exc, tb):
+        if self._owner is not None and self._attr_name is not None:
+            setattr(self._owner, self._attr_name, self._original)
+        return False
+
+
+intercept = _Intercept
