fix: Fixed int type issue for kernel sparsity computation

torchstream/sliding_window/kernel_sparsity.py
@@ -2,7 +2,7 @@ import logging
 from typing import Tuple
 
 import numpy as np
-from z3 import UGT, BitVec, BitVecVal, Extract, Or, Solver, unsat
+from z3 import UGT, BitVec, BitVecVal, Extract, If, Solver, unsat
 
 from torchstream.sliding_window.sliding_window_params import SlidingWindowParams
 
@@ -35,6 +35,10 @@ def get_init_kernel_array(kernel_size: int, full: bool = False) -> np.ndarray:
 
 def np_to_bitvec(arr: np.ndarray, set_value: int) -> BitVec:
     return BitVecVal(sum((1 << i) for i, bit in enumerate(arr) if bit == set_value), arr.size)
+
+
+def bitvec_to_str(bv: BitVec) -> str:
+    return format(bv.as_long(), f"0{bv.size()}b")[::-1]
 
 
 def get_nan_map(
@@ -114,12 +118,14 @@ class KernelSparsitySampler:
         )
 
     def add_in_out_map(self, in_len: int, in_nan_range: Tuple[int, int], out_nan_idx: np.ndarray):
+        in_nan_range = (int(in_nan_range[0]), int(in_nan_range[1]))
+
         # Encode each window being corrupted as a boolean variable
         _, num_wins, out_len = self.params.get_metrics_for_input(in_len)
         var_id = len(self.solver.assertions())
         corrupted_wins = BitVec(f"cw_{var_id}", num_wins)
 
-        out_nan_mask = sum(1 << idx for idx in out_nan_idx if 0 <= idx < out_len)
+        out_nan_mask = sum(1 << int(idx) for idx in out_nan_idx)
         out_nan_vec = BitVecVal(out_nan_mask, out_len)
 
         for win_idx, ((in_start, in_stop), (out_start, out_stop)) in enumerate(self.params.iter_kernel_map(num_wins)):
@@ -127,8 +133,8 @@ class KernelSparsitySampler:
 
             # The kernel can only output nans (=be corrupted) if it has any overlap with the input nans
             kernel_in_nan_range = (
-                int(max(in_nan_range[0], in_start) - in_start),
-                int(min(in_nan_range[1], in_stop) - in_start),
+                max(in_nan_range[0], in_start) - in_start,
+                min(in_nan_range[1], in_stop) - in_start,
             )
             if (
                 in_nan_range[0] < in_stop
@@ -143,30 +149,21 @@ class KernelSparsitySampler:
                 self.solver.add(corrupted_wins & win_mask == 0)
 
         for out_start, out_end, overlapping_wins in self.params.get_inverse_kernel_map(in_len):
-            out_nan_slice = Extract(out_end - 1, out_start, out_nan_vec)
-
             nan_kernel_slices = [
-                Or(
+                If(
                     # TODO: maybe let's not use bitvecs for the corrupted_wins?
                     corrupted_wins & (1 << win_idx) != 0,
                     Extract(kernel_out_stop - 1, kernel_out_start, self._kernel_out),
-                    BitVecVal(0, kernel_out_stop - kernel_out_start)
+                    BitVecVal(0, kernel_out_stop - kernel_out_start),
                 )
                 for win_idx, kernel_out_start, kernel_out_stop in overlapping_wins
             ]
 
-            #     # TODO: optimize
-            # for out_idx in range(out_start, out_end):
-            #     any_corrupted_constraint = Or(
-            #         *[
-            #             And(
-            #                 (corrupted_wins & (1 << win_idx)) != 0,
-            #                 (self._kernel_out & (1 << (kernel_out_start + out_idx - out_start))) != 0,
-            #             )
-            #             for win_idx, kernel_out_start, kernel_out_stop in overlapping_wins
-            #         ]
-            #     )
-            #     self.solver.add(any_corrupted_constraint if out_idx in out_nan_idx else Not(any_corrupted_constraint))
+            out_nan_slice = nan_kernel_slices[0]
+            for nan_kernel_slice in nan_kernel_slices[1:]:
+                out_nan_slice |= nan_kernel_slice
+
+            self.solver.add(out_nan_slice == Extract(out_end - 1, out_start, out_nan_vec))
 
     def has_solution(self) -> bool:
         # If the solver can't find any solution, then the parameters do not allow to explain the observed nans