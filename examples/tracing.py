20939623cbabae4c0defe0e594caa61c73bf3deb
CorentinJ <corentin.jemine@gmail.com>
2025-11-03 11:21:10 +0100

    feat: Demo utility function to profile with tracing

--- examples/tracing.py
+++ examples/tracing.py
@@ -1,10 +1,10 @@
 import logging
-import time
 import typing
 from contextlib import AbstractContextManager
 from functools import lru_cache
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
+import numpy as np
 from opentelemetry import trace
 from opentelemetry.sdk.trace import ReadableSpan, TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, SpanExporter, SpanExportResult
@@ -12,10 +12,11 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor, SpanExporter, Spa
 logger = logging.getLogger(__name__)
 
 
-class log_tracing_statistics(AbstractContextManager):
-    def __init__(self):
+class log_tracing_profile(AbstractContextManager):
+    def __init__(self, name: str = "ctx-manager"):
+        self.name = name
         self._root_span_cm = None
-        self._span_processor = BatchSpanProcessor(LogsProfilerSpanExporter())
+        self._span_processor = BatchSpanProcessor(LogsProfilerSpanExporter(name))
 
     def __enter__(self):
         # Retrieve/create tracer provider
@@ -27,7 +28,7 @@ class log_tracing_statistics(AbstractContextManager):
         # FIXME accumulation of processors
         provider.add_span_processor(self._span_processor)
 
-        self._root_span_cm = tracer.start_as_current_span("demo-root")
+        self._root_span_cm = tracer.start_as_current_span(self.name)
         self._root_span_cm.__enter__()
 
         return self
@@ -43,18 +44,19 @@ class log_tracing_statistics(AbstractContextManager):
         return False
 
 
-def _build_profiler_tree(spans: typing.Sequence[ReadableSpan]) -> Dict[str, List[ReadableSpan]]:
+def _group_spans_by_hierarchy(spans: typing.Sequence[ReadableSpan]) -> Dict[str, List[ReadableSpan]]:
     tree = {}
 
     @lru_cache(maxsize=None)
     def span_id_to_name(span_id: str) -> str:
-        span = next((s for s in spans if s.span_id == span_id), None)
+        span = next(s for s in spans if s.context.span_id == span_id)
+        span_name = span.name.replace(".", "_")
         if span.parent is None:
-            return span.name
-        return span_id_to_name(span.parent.span_id) + "." + span.name
+            return span_name
+        return span_id_to_name(span.parent.span_id) + "." + span_name
 
     for span in spans:
-        tree.setdefault(span_id_to_name(span.span_id), []).append(span)
+        tree.setdefault(span_id_to_name(span.context.span_id), []).append(span)
 
     return tree
 
@@ -62,33 +64,44 @@ def _build_profiler_tree(spans: typing.Sequence[ReadableSpan]) -> Dict[str, List
 class LogsProfilerSpanExporter(SpanExporter):
     def __init__(
         self,
+        root_span_name: str,
         min_display_dur_ms=0,
         log_gaps_longer_than_ms=None,
     ):
+        self.root_span_name = root_span_name
         self.min_display_dur_s = min_display_dur_ms / 1000
         self.min_gap_dur_s = float("inf") if log_gaps_longer_than_ms is None else log_gaps_longer_than_ms / 1000
 
     def export(self, spans: typing.Sequence[ReadableSpan]) -> SpanExportResult:
-        root_entry = _build_profiler_tree(spans)
+        name2spans = _group_spans_by_hierarchy(spans)
+
+        def build_log_table(span_name) -> List[Tuple[str, ...]]:
+            depth = span_name.count(".") + 1
+            line = []
+            line.append("  " * depth + span_name.split(".")[-1])
+            line.append(f"  ({len(name2spans[span_name])}x)")
+            durations_ms = [(s.end_time - s.start_time) / 1e6 for s in name2spans[span_name]]
+            line.extend(("  ", "total: ", f"{np.sum(durations_ms):.0f}ms"))
+            if len(durations_ms) > 1:
+                line.extend(("  mean: ", f"{np.mean(durations_ms):.0f}ms"))
+                line.extend(("  std: ", f"{np.std(durations_ms):.0f}ms"))
+            else:
+                line.extend([""] * 4)
+
+            children_names = [
+                name for name in name2spans.keys() if name.startswith(span_name + ".") and name.count(".") == depth
+            ]
+
+            return [tuple(line)] + [line for child_name in children_names for line in build_log_table(child_name)]
+
+        table = build_log_table(self.root_span_name)
+
+        # Align all columns
+        max_lens = [max(len(row[i]) for row in table) for i in range(len(table[0]))]
+        log_message = "\n".join(
+            "".join(((row[i].rjust if i > 0 else row[i].ljust)(max_lens[i]) for i in range(len(row)))) for row in table
+        )
+
+        logger.info("Profile from tracing:\n" + log_message)
 
         return SpanExportResult.SUCCESS
-
-
-tracer = trace.get_tracer("torchstream.demo")
-
-with log_tracing_statistics():
-    with tracer.start_as_current_span("demo-request") as request_span:
-        request_span.set_attribute("demo.request_id", "example-1")
-        request_span.add_event("request.received")
-
-        with tracer.start_as_current_span("load-model") as load_span:
-            load_span.add_event("loading.start")
-            time.sleep(0.05)
-            load_span.add_event("loading.completed", {"result": "cached"})
-
-        with tracer.start_as_current_span("run-model") as run_span:
-            run_span.set_attribute("batch.size", 3)
-            time.sleep(0.08)
-            run_span.add_event("model.output", {"tokens": 128})
-
-        request_span.add_event("request.completed")
