47f6867e383cd15ad087f09276ce06107f63a9ad
CorentinJ <corentin.jemine@gmail.com>
2025-11-25 09:37:22 +0100

    feat: Intro done for the melspec demo

--- demo_tools/audio.py
+++ demo_tools/audio.py
@@ -2,17 +2,18 @@ from pathlib import Path
 from typing import Tuple
 
 import audioread
+import librosa
 import numpy as np
 
 
-def load_audio(path: Path) -> Tuple[np.ndarray, int]:
+def load_audio(path: Path, sample_rate=None) -> Tuple[np.ndarray, int]:
     with audioread.audio_open(str(path)) as f:
-        sample_rate = f.samplerate
+        orig_sample_rate = f.samplerate
         channels = f.channels
         pcm_chunks = [np.frombuffer(buf, dtype=np.int16) for buf in f]
 
     if not pcm_chunks:
-        return np.empty(0, dtype=np.float32), sample_rate
+        return np.empty(0, dtype=np.float32), orig_sample_rate
 
     audio = np.concatenate(pcm_chunks)
 
@@ -22,4 +23,8 @@ def load_audio(path: Path) -> Tuple[np.ndarray, int]:
 
     wave = audio.astype(np.float32) / 32768.0
 
-    return wave, sample_rate
+    if sample_rate:
+        wave = librosa.resample(wave, orig_sr=orig_sample_rate, target_sr=sample_rate)
+        orig_sample_rate = sample_rate
+
+    return wave, orig_sample_rate
--- examples/melspectrogram/script.py
+++ examples/melspectrogram/script.py
@@ -1,3 +1,4 @@
+import inspect
 import logging
 
 import matplotlib.pyplot as plt
@@ -19,15 +20,40 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
+st.subheader("1. TorchStream Introduction using Mel-Spectrograms")
+
+"""
+A very common speech processing data transformation is the Mel-Spectrogram. ML models that generate or ingest speech 
+will typically represent it as a Mel-Spectrogram.
+"""
+st.space(1)
+"""
+Let's take a short speech sample from the web:
+"""
 MP3_URL = "https://d38nvwmjovqyq6.cloudfront.net/va90web25003/companions/ws_smith/32%20Speaking%20The%20Text%20As%20A%20Dramatic%20Reading.mp3"
-
-
 local_audio_path = download_file_cached(MP3_URL)
-
-
 wave, sample_rate = load_audio(local_audio_path)
 st.audio(wave, sample_rate=sample_rate)
-wave = torch.from_numpy(wave)
+st.caption("Source: https://global.oup.com/us/companion.websites/9780195300505/audio/audio_samples/, sample 32")
+
+
+def plot_audio(ax, wave: np.ndarray, sample_rate: int):
+    time_axis = np.arange(wave.shape[0]) / sample_rate
+    ax.plot(time_axis, wave)
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Amplitude")
+    ax.set_xlim(0, time_axis[-1])
+    ax.set_ylim(-1, 1)
+
+
+fig, ax = plt.subplots(figsize=(10, 2.5))
+plot_audio(ax, wave, sample_rate)
+st.pyplot(fig)
+
+"""
+Torchaudio provides a function to compute Mel-Spectrograms from an audio. Let's wrap it in a function with a 
+couple useful parameters
+"""
 
 
 def get_spectrogram(
@@ -44,21 +70,50 @@ def get_spectrogram(
         center=center,
         n_mels=n_mels,
         hop_length=hop_size,
-    )(wave)
+    )(torch.from_numpy(wave))
 
 
-def plot_melspec(spec, title="MelSpectrogram"):
-    fig, ax = plt.subplots(figsize=(12, 3))
+st.code(inspect.getsource(get_spectrogram))
+
+
+"""
+The output of that function is a (n_mels, n_frames) shaped tensor that we can view as a 2D image:
+"""
+
+
+def plot_melspec(ax, spec, title="MelSpectrogram"):
     ax.imshow(spec.log2().numpy(), aspect="auto", origin="lower")
     ax.set_title(title)
     ax.set_xlabel("Frames")
     ax.set_ylabel("Mel Bin")
-    st.pyplot(fig)
+    ax.set_xlim(0, spec.shape[1])
+    ax.set_ylim(0, spec.shape[0])
 
 
-plot_melspec(get_spectrogram(wave, sample_rate))
+fig, ax = plt.subplots(figsize=(10, 2.5))
+plot_melspec(ax, get_spectrogram(wave, sample_rate))
+st.pyplot(fig)
+
+"""
+And if you're a speech or DSP expert, you could tell from this image alone that this is indeed human speech, from a 
+single speaker, with normal prosody, recorded with little noise in the room at a high sample rate.
+"""
+
+
 quit()
 
+"""
+An audio file 
+"""
+# TODO! sharex
+
+st.code(f"{wave.shape} shaped {wave.dtype} numpy array\n{wave[sample_rate : sample_rate + 5]}...")
+fig, axs = plt.subplots(figsize=(10, 5.5), nrows=2)
+fig.subplots_adjust(hspace=0.5)
+axs[0].set_title("Audio")
+plot_audio(axs[0], wave, sample_rate)
+plot_melspec(axs[1], get_spectrogram(wave, sample_rate))
+st.pyplot(fig)
 
 in_spec = SeqSpec(-1)
 out_spec = SeqSpec(n_mels, -1)
