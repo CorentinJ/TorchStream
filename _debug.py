fa44595042979e53f81f137fcdb9ebbdb746ce2c
CorentinJ <corentin.jemine@gmail.com>
2025-12-01 20:47:57 +0100

    docs(tut3): Working bigvgan example with weights

--- _debug.py
+++ _debug.py
@@ -1,9 +1,10 @@
-import librosa
+import streamlit as st
 import torch
 
 from examples.resources.bigvgan import bigvgan
-
-# from meldataset import get_mel_spectrogram
+from examples.resources.bigvgan.melspectrogram import get_mel_spectrogram
+from examples.utils.audio import load_audio
+from examples.utils.download import download_file_cached
 
 device = "cuda"
 
@@ -15,10 +16,9 @@ model.remove_weight_norm()
 model = model.eval().to(device)
 
 # load wav file and compute mel spectrogram
-wav_path = "/path/to/your/audio.wav"
-wav, sr = librosa.load(
-    wav_path, sr=model.h.sampling_rate, mono=True
-)  # wav is np.ndarray with shape [T_time] and values in [-1, 1]
+MP3_URL = "https://d38nvwmjovqyq6.cloudfront.net/va90web25003/companions/ws_smith/32%20Speaking%20The%20Text%20As%20A%20Dramatic%20Reading.mp3"
+local_audio_path = download_file_cached(MP3_URL)
+wav, sr = load_audio(local_audio_path, sample_rate=model.h.sampling_rate)
 wav = torch.FloatTensor(wav).unsqueeze(0)  # wav is FloatTensor with shape [B(1), T_time]
 
 # compute mel spectrogram from the ground truth audio
@@ -29,7 +29,4 @@ with torch.inference_mode():
     wav_gen = model(mel)  # wav_gen is FloatTensor with shape [B(1), 1, T_time] and values in [-1, 1]
 wav_gen_float = wav_gen.squeeze(0).cpu()  # wav_gen is FloatTensor with shape [1, T_time]
 
-# you can convert the generated waveform to 16 bit linear PCM
-wav_gen_int16 = (
-    (wav_gen_float * 32767.0).numpy().astype("int16")
-)  # wav_gen is now np.ndarray with shape [1, T_time] and int16 dtype
+st.audio(wav_gen_float.numpy(), sample_rate=model.h.sampling_rate)
