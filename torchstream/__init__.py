933d26e312ed7a6286f0e7d3355adaa95f5ca7f1
CorentinJ <corentin.jemine@gmail.com>
2025-12-02 14:57:24 +0100

    refactor: Revised which members to expose in the API

--- torchstream/__init__.py
+++ torchstream/__init__.py
@@ -1,10 +1,7 @@
-from torchstream.exception_signature import (
-    DEFAULT_ZERO_SIZE_EXCEPTIONS,
-    matches_any_exception,
-)
-from torchstream.patching.call_intercept import make_exit_early, intercept_calls
-from torchstream.sequence.array_interface import ArrayInterface, NumpyArrayInterface, SeqArray, TensorInterface
-from torchstream.sequence.dtype import DeviceLike, SeqArrayLike, SeqDTypeLike, dtypes_compatible, seqdtype, to_seqdtype
+from torchstream.exception_signature import DEFAULT_ZERO_SIZE_EXCEPTIONS
+from torchstream.patching.call_intercept import intercept_calls, make_exit_early
+from torchstream.sequence.array_interface import SeqArray
+from torchstream.sequence.dtype import DeviceLike, SeqArrayLike, SeqDTypeLike
 from torchstream.sequence.seq_spec import SeqSpec
 from torchstream.sequence.sequence import Sequence
 from torchstream.sliding_window.sliding_window_params import SlidingWindowParams
@@ -14,7 +11,6 @@ from torchstream.sliding_window.sliding_window_params_solver import (
     find_sliding_window_params,
 )
 from torchstream.sliding_window.sliding_window_stream import IncorrectSlidingWindowParametersError, SlidingWindowStream
-from torchstream.sliding_window.transforms import overlap_windows, run_sliding_window, view_as_windows
 from torchstream.stream import NotEnoughInputError, Stream
 from torchstream.stream_equivalence import test_stream_equivalent
 
@@ -29,22 +25,12 @@ __all__ = [
     "SlidingWindowStream",
     "intercept_calls",
     "make_exit_early",
-    "ArrayInterface",
-    "TensorInterface",
-    "NumpyArrayInterface",
     "SeqArray",
     "SeqDTypeLike",
     "SeqArrayLike",
     "DeviceLike",
-    "seqdtype",
-    "to_seqdtype",
-    "dtypes_compatible",
     "DEFAULT_ZERO_SIZE_EXCEPTIONS",
-    "matches_any_exception",
     "NotEnoughInputError",
     "MaximumSequenceSizeReachedError",
     "IncorrectSlidingWindowParametersError",
-    "view_as_windows",
-    "overlap_windows",
-    "run_sliding_window",
 ]
